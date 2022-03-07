01cc18fbcbb1933ecbd86dfb652c78878cdccd9d
Adelina <59620862+adelina15@users.noreply.github.com>
2022-03-08 01:31:00 +0600

    Added order details

--- app/src/main/java/com/example/omela/model/SaleItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.omela.model
-
-data class SaleItem(
-    val flower_name: String,
-    val flower_image: Int,
-    val flower_price: Int,
-    val discount_rate: String,
-    val is_favorite: Boolean = false
-)
